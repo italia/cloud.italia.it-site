38c567e55e44ee3e1b389906b149e963bbac4ba0
emilioSp <emilio.spatola@gmail.com>
2021-02-22 11:53:30 +0100

    fix: focus behaviour

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -5,28 +5,41 @@ import 'typeface-titillium-web';
 import 'typeface-roboto-mono';
 import 'typeface-lora';
 import 'bootstrap-italia/dist/css/bootstrap-italia.min.css';
+import { createUseStyles } from 'react-jss';
 import favicon from '../images/favicon.ico';
 import { Header } from './Header.js';
 import { Footer } from './Footer.js';
 
-export const Layout = ({ children }) => (
-  <>
-    <Helmet
-      title="Cloud Italia"
-      meta={[
-        { name: 'viewport', content: 'width=device-width, initial-scale=1.0' },
-        { name: 'description', content: 'Il Cloud della Pubblica Amministrazione' },
-      ]}
-    >
-      <html lang="it" />
-      <link rel="icon" href={favicon} />
-      <noscript>Il tuo browser non supporta JavaScript</noscript>
-    </Helmet>
-    <Header />
-    {children}
-    <Footer />
-  </>
-);
+const useStyles = createUseStyles({
+  '@global': {
+    ':focus:not(:focus-visible)': {
+      borderColor: 'initial',
+      boxShadow: 'none',
+    },
+  },
+});
+
+export const Layout = ({ children }) => {
+  useStyles();
+  return (
+    <>
+      <Helmet
+        title="Cloud Italia"
+        meta={[
+          { name: 'viewport', content: 'width=device-width, initial-scale=1.0' },
+          { name: 'description', content: 'Il Cloud della Pubblica Amministrazione' },
+        ]}
+      >
+        <html lang="it" />
+        <link rel="icon" href={favicon} />
+        <noscript>Il tuo browser non supporta JavaScript</noscript>
+      </Helmet>
+      <Header />
+      {children}
+      <Footer />
+    </>
+  );
+};
 
 Layout.propTypes = {
   children: PropTypes.node.isRequired,
