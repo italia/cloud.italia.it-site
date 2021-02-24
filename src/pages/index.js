db3416015e019f4c9e7e3cb9f1fdd6336ce9dc45
emilioSp <emilio.spatola@gmail.com>
2021-02-24 18:43:09 +0100

    feat: swiper (wip)

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,10 +2,17 @@ import React from 'react';
 import { graphql, Link } from 'gatsby';
 import PropTypes from 'prop-types';
 import { createUseStyles } from 'react-jss';
-import { Icon } from 'design-react-kit';
+import { Card, CardBody, CardText, Icon } from 'design-react-kit';
+
+import SwiperCore, { Pagination, A11y } from 'swiper';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import 'swiper/swiper-bundle.min.css';
+
 import { Hero } from '../components/Hero.js';
 import { HeroImage } from '../components/HeroImage.js';
 import { imageSharpProptype } from '../utils/proptypes.js';
+
+SwiperCore.use([Pagination, A11y]);
 
 const buttonClasses = 'btn text-uppercase btn-primary mx-4 ml-lg-0 my-2 my-md-0';
 const useStyles = createUseStyles({
@@ -97,6 +104,112 @@ const Index = ({ data }) => {
           </>
         )}
       />
+      <div className="pb-5 lightgrey-bg-a2">
+        <div className="container">
+          <h2 className="h6 text-uppercase text-center text-lg-left pt-5">Come contribuire</h2>
+          <h3 className="text-center text-lg-left py-3 col-lg-5">
+            Partecipa al cambiamento, contribuisci a Cloud italia!
+          </h3>
+          <div className="row">
+            <div className="card-wrapper card-teaser-wrapper card-teaser-wrapper-equal card-teaser-block-3 d-none d-lg-flex">
+              <Card teaser noWrapper className="no-after rounded shadow-lg">
+                <CardBody className="pb-5">
+                  <div className="mb-3 d-flex align-items-center">
+                    <Icon color="primary" icon="it-github" size="lg" />
+                    <span className="primary-color px-3 h3 mb-0">GitHub</span>
+                  </div>
+                  <CardText>
+                    In <strong>forum.italia.it</strong> si discute dei servizi pubblici digitali: piattaforme, API,
+                    software, sicurezza ed altro. Unisciti alla discussione!
+                  </CardText>
+                </CardBody>
+              </Card>
+              <Card teaser noWrapper className="no-after rounded shadow-lg">
+                <CardBody className="pb-5">
+                  <div className="mb-3 d-flex align-items-center">
+                    <Icon color="primary" icon="it-star-full" size="lg" />
+                    <span className="primary-color px-3 h3 mb-0">slack</span>
+                  </div>
+                  <CardText>
+                    In <strong>forum.italia.it</strong> si discute dei servizi pubblici digitali: piattaforme, API,
+                    software, sicurezza ed altro. Unisciti alla discussione!
+                  </CardText>
+                </CardBody>
+              </Card>
+              <Card teaser noWrapper className="no-after rounded shadow-lg">
+                <CardBody className="pb-5">
+                  <div className="mb-3 d-flex align-items-center">
+                    <Icon color="primary" icon="it-designers-italia" size="lg" />
+                    <span className="primary-color px-3 h3 mb-0">Forum</span>
+                  </div>
+                  <CardText>
+                    In <strong>forum.italia.it</strong> si discute dei servizi pubblici digitali: piattaforme, API,
+                    software, sicurezza ed altro. Unisciti alla discussione!
+                  </CardText>
+                </CardBody>
+              </Card>
+            </div>
+            <div className="col-12 d-lg-none">
+              <Swiper
+                a11y={{
+                  enabled: true,
+                  prevSlideMessage: 'Slide precedente',
+                  nextSlideMessage: 'Slide successiva',
+                  firstSlideMessage: 'Questa è la prima slide',
+                  lastSlideMessage: "Questa è l'ultima slide",
+                  paginationBulletMessage: 'Vai alla slide {{index}}',
+                }}
+                className="shadow-lg"
+                slidesPerView={1}
+                pagination={{ clickable: true, bulletClass: 'swiper-pagination-bullet p-2 mx-3' }}
+              >
+                <SwiperSlide>
+                  <Card teaser noWrapper className="rounded">
+                    <CardBody className="pb-5">
+                      <div className="mb-3 d-flex align-items-center">
+                        <Icon color="primary" icon="it-github" size="lg" />
+                        <span className="primary-color px-3 h3 mb-0">GitHub</span>
+                      </div>
+                      <CardText>
+                        In <strong>forum.italia.it</strong> si discute dei servizi pubblici digitali: piattaforme, API,
+                        software, sicurezza ed altro. Unisciti alla discussione!
+                      </CardText>
+                    </CardBody>
+                  </Card>
+                </SwiperSlide>
+                <SwiperSlide>
+                  <Card teaser noWrapper className="rounded">
+                    <CardBody className="pb-5">
+                      <div className="mb-3 d-flex align-items-center">
+                        <Icon color="primary" icon="it-star-full" size="lg" />
+                        <span className="primary-color px-3 h3 mb-0">slack</span>
+                      </div>
+                      <CardText>
+                        In <strong>forum.italia.it</strong> si discute dei servizi pubblici digitali: piattaforme, API,
+                        software, sicurezza ed altro. Unisciti alla discussione!
+                      </CardText>
+                    </CardBody>
+                  </Card>
+                </SwiperSlide>
+                <SwiperSlide>
+                  <Card teaser noWrapper className="rounded">
+                    <CardBody className="pb-5">
+                      <div className="mb-3 d-flex align-items-center">
+                        <Icon color="primary" icon="it-designers-italia" size="lg" />
+                        <span className="primary-color px-3 h3 mb-0">Forum</span>
+                      </div>
+                      <CardText>
+                        In <strong>forum.italia.it</strong> si discute dei servizi pubblici digitali: piattaforme, API,
+                        software, sicurezza ed altro. Unisciti alla discussione!
+                      </CardText>
+                    </CardBody>
+                  </Card>
+                </SwiperSlide>
+              </Swiper>
+            </div>
+          </div>
+        </div>
+      </div>
     </main>
   );
 };
--- src/pages/strategia.js
+++ src/pages/strategia.js
@@ -1,8 +1,93 @@
 import React from 'react';
+import SwiperCore, { Pagination, A11y } from 'swiper';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import 'swiper/swiper-bundle.min.css';
+
+SwiperCore.use([Pagination, A11y]);
 
 const Strategia = () => (
   <main className="container">
     <div>Strategia Nazionale</div>
+    <Swiper
+      a11y={{
+        enabled: true,
+        prevSlideMessage: 'Slide precedente',
+        nextSlideMessage: 'Slide successiva',
+        firstSlideMessage: 'Questa è la prima slide',
+        lastSlideMessage: "Questa è l'ultima slide",
+        paginationBulletMessage: 'Vai alla slide {{index}}',
+      }}
+      className="d-lg-none"
+      slidesPerView={1}
+      pagination={{ clickable: true, bulletClass: 'swiper-pagination-bullet p-2 mx-3' }}
+    >
+      <SwiperSlide className="my-5">
+        <div className="card rounded ">
+          <div className="card-body d-flex flex-column">
+            <p className="card-text text-secondary">
+              <span>16 gennaio 2021</span>
+            </p>
+            <h3 className="h4 text-primary mid-caption--xlarge font-weight-bold">
+              <a
+                href="/notizie/comunicati-stampa/smarter-italy-al-via-borghi-del-futuro"
+                className="text-decoration-none"
+              >
+                Smarter Italy: al via “Borghi del futuro”
+              </a>
+            </h3>
+            <p className="pt-2 mid-caption--large">
+              Il programma entra nel vivo con l’adesione di 12 borghi dove sperimentare soluzioni tecnologiche
+              emergenti
+            </p>
+          </div>
+          <div className="mt-auto d-flex flex-wrap-reverse flex-row-reverse align-items-end"></div>
+        </div>
+      </SwiperSlide>
+      <SwiperSlide className="my-5">
+        <div className="card rounded ">
+          <div className="card-body d-flex flex-column">
+            <p className="card-text text-secondary">
+              <span>16 gennaio 2021</span>
+            </p>
+            <h3 className="h4 text-primary mid-caption--xlarge font-weight-bold">
+              <a
+                href="/notizie/comunicati-stampa/smarter-italy-al-via-borghi-del-futuro"
+                className="text-decoration-none"
+              >
+                Smarter Italy: al via “Borghi del futuro”
+              </a>
+            </h3>
+            <p className="pt-2 mid-caption--large">
+              Il programma entra nel vivo con l’adesione di 12 borghi dove sperimentare soluzioni tecnologiche
+              emergenti
+            </p>
+          </div>
+          <div className="mt-auto d-flex flex-wrap-reverse flex-row-reverse align-items-end"></div>
+        </div>
+      </SwiperSlide>
+      <SwiperSlide className="my-5">
+        <div className="card rounded ">
+          <div className="card-body d-flex flex-column">
+            <p className="card-text text-secondary">
+              <span>16 gennaio 2021</span>
+            </p>
+            <h3 className="h4 text-primary mid-caption--xlarge font-weight-bold">
+              <a
+                href="/notizie/comunicati-stampa/smarter-italy-al-via-borghi-del-futuro"
+                className="text-decoration-none"
+              >
+                Smarter Italy: al via “Borghi del futuro”
+              </a>
+            </h3>
+            <p className="pt-2 mid-caption--large">
+              Il programma entra nel vivo con l’adesione di 12 borghi dove sperimentare soluzioni tecnologiche
+              emergenti
+            </p>
+          </div>
+          <div className="mt-auto d-flex flex-wrap-reverse flex-row-reverse align-items-end"></div>
+        </div>
+      </SwiperSlide>
+    </Swiper>
   </main>
 );
 
