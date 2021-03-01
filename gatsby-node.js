5bb9b2ddeee1475db0129f7ac239600c8f18961d
emilioSp <emilio.spatola@gmail.com>
2021-03-01 14:28:12 +0100

    feat: source content from md data

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,27 +1,44 @@
 const path = require('path');
 const { createFilePath } = require(`gatsby-source-filesystem`);
 
+// TODO: quando i contenuti saranno disponibili, controllare se c'è veramente bisogno di md di tipo news e data
+
 exports.onCreateNode = ({ node, getNode, actions }) => {
   const { createNodeField } = actions;
+
   // Ensures we are processing only markdown files
   if (node.internal.type === 'MarkdownRemark') {
-    // Use `createFilePath` to turn markdown files in our `src/news` directory into `/notizie/slug`
-    const relativeFilePath = createFilePath({
-      node,
-      getNode,
-      basePath: 'src/news/',
-    });
+    // Markdown file could be an entire page (news) or a content inside a page (data)
 
-    // Create a default value for show_in_home in frontmatter
-    if (!Object.prototype.hasOwnProperty.call(node.frontmatter, 'show_in_home')) {
-      node.frontmatter = { ...node.frontmatter, show_in_home: false };
+    // Get the parent node
+    const parent = getNode(node.parent);
+
+    if (parent.sourceInstanceName === 'news') {
+      // Use `createFilePath` to turn markdown files in our `src/news` directory into `/notizie/slug`
+      const relativeFilePath = createFilePath({
+        node,
+        getNode,
+        basePath: 'src/news/',
+      });
+
+      // Create a default value for show_in_home in frontmatter
+      if (!Object.prototype.hasOwnProperty.call(node.frontmatter, 'show_in_home')) {
+        node.frontmatter = { ...node.frontmatter, show_in_home: false };
+      }
+
+      // Creates new query'able field with name of 'path'
+      createNodeField({
+        node,
+        name: 'slug',
+        value: `/notizie${relativeFilePath}`,
+      });
     }
 
-    // Creates new query'able field with name of 'path'
+    // NOTE: This is necessary so we can filter `allMarkdownRemark` by `type` and create pages only for news type md
     createNodeField({
       node,
-      name: 'slug',
-      value: `/notizie${relativeFilePath}`,
+      name: 'type',
+      value: parent.sourceInstanceName,
     });
   }
 };
@@ -29,7 +46,7 @@ exports.onCreateNode = ({ node, getNode, actions }) => {
 exports.createPages = async ({ actions: { createPage }, graphql }) => {
   const result = await graphql(`
     {
-      allMarkdownRemark {
+      allMarkdownRemark(filter: { fields: { type: { eq: "news" } } }) {
         edges {
           node {
             fields {
--- src/pages/adozione.js
+++ src/pages/adozione.js
@@ -1,9 +1,32 @@
 import React from 'react';
+import { graphql, useStaticQuery } from 'gatsby';
 
-const Adozione = () => (
-  <main className="container">
-    <div>Programma di adozione</div>
-  </main>
-);
+const Adozione = () => {
+  const {
+    allMarkdownRemark: {
+      nodes: [data],
+    },
+  } = useStaticQuery(graphql`
+    query {
+      allMarkdownRemark(filter: { fields: { type: { eq: "data" } }, frontmatter: { title: { eq: "md interno" } } }) {
+        nodes {
+          html
+        }
+      }
+    }
+  `);
+  return (
+    <main className="container">
+      <h2>Testo scritto in md</h2>
+
+      <div className="row align-items-center">
+        <div
+          className="col-lg-6 offset-lg-3 col-md-8 offset-md-2 col-sm-12 offset-0"
+          dangerouslySetInnerHTML={{ __html: data.html }}
+        ></div>
+      </div>
+    </main>
+  );
+};
 
 export default Adozione;
