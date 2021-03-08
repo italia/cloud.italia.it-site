a21cf3df58aaea387a1dafc87cf62e4a42b2e73d
Emilio Spatola <emilio.spatola@gmail.com>
2021-03-08 18:41:31 +0100

    feat: redirect with canonical url (#7)

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const fsPromises = require('fs/promises');
 const { createFilePath } = require(`gatsby-source-filesystem`);
 
 // TODO: quando i contenuti saranno disponibili, controllare se c'è veramente bisogno di md di tipo news e data
@@ -49,13 +50,7 @@ exports.onCreateNode = ({ node, getNode, actions }) => {
   }
 };
 
-exports.createPages = async ({ actions: { createPage, createRedirect }, graphql }) => {
-  createRedirect({
-    fromPath: '/it/cloud-enablement/',
-    toPath: '/programma-abilitazione-cloud/',
-    statusCode: 301,
-  });
-
+exports.createPages = async ({ actions: { createPage }, graphql }) => {
   const result = await graphql(`
     {
       allMarkdownRemark(filter: { fields: { type: { eq: "news" } } }) {
@@ -75,10 +70,45 @@ exports.createPages = async ({ actions: { createPage, createRedirect }, graphql 
     throw new Error(result.errors);
   }
 
-  return result.data.allMarkdownRemark.edges.forEach(({ node }) => {
+  result.data.allMarkdownRemark.edges.forEach(({ node }) => {
     createPage({
       path: node.fields.slug,
-      component: path.resolve(__dirname, 'src/templates/News.js'),
+      component: path.join(__dirname, 'src/templates/News.js'),
     });
   });
+};
+
+exports.onPostBuild = async () => {
+  /*
+   * These are a collection of redirects. Some are needed due to the restyling of the site, others are required because are used by AgID
+   */
+  const redirects = [
+    { from: '/it/cloud-enablement/', to: '/programma-abilitazione-cloud/' },
+    { from: '/it/qualificazioni/', to: '/qualificazione-servizi-cloud/' },
+    { from: '/it/latest/', to: '/' },
+    {
+      from: '/projects/cloud-italia-circolari/it/latest/',
+      to: 'https://cloud-italia.readthedocs.io/projects/cloud-italia-circolari/it/latest/',
+    },
+    {
+      from: '/projects/cloud-italia-docs/it/latest',
+      to: 'https://docs.italia.it/italia/piano-triennale-ict/cloud-docs/',
+    },
+  ];
+
+  for (const redirect of redirects) {
+    const { from, to } = redirect;
+    const redirectFile = path.join(__dirname, 'public', from, 'index.html');
+    await fsPromises.mkdir(path.dirname(redirectFile), {
+      recursive: true,
+    });
+    await fsPromises.writeFile(
+      redirectFile,
+      `<!DOCTYPE html>
+<meta charset="utf-8">
+<title>Redirecting to ${to}</title>
+<meta http-equiv="refresh" content="0; URL=${to}">
+<link rel="canonical" href="${to}">`
+    );
+  }
 };
