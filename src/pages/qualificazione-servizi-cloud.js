d6e68f963a92599a625908f3e37a9e98a82ed781
Emilio Spatola <emilio.spatola@gmail.com>
2021-03-15 12:31:44 +0100

    fix: page headings (#37)

--- src/pages/qualificazione-servizi-cloud.js
+++ src/pages/qualificazione-servizi-cloud.js
@@ -3,6 +3,7 @@ import { Timeline } from '../components/Timeline.js';
 
 const ServicesPage = () => (
   <>
+    <h1 className="sr-only">Qaulificazione dei servizi</h1>
     <div className="section lightgrey-bg-a2">
       <h2 className="h4 text-center text-primary">Piano di attuazione</h2>
       <Timeline />
