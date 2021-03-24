8ca91889da14888453c91735d9d29e99d2639c32
emilioSp <emilio.spatola@gmail.com>
2021-03-24 16:58:39 +0100

    fix: repository pubblici aria-label fix: logos in how to contribute section

--- src/layouts/home/HowToContributeSection.js
+++ src/layouts/home/HowToContributeSection.js
@@ -12,17 +12,18 @@ const {
 
 const useStyles = createUseStyles({
   logo: {
-    zoom: 0.5,
+    height: '35px',
   },
 });
 
 export const HowToContributeSection = () => {
   const classes = useStyles();
   const slides = contributions.map((card) => (
-    <Card key={card.icon} teaser noWrapper className="rounded shadow-lg col-lg-3 col-12 mr-4">
+    <Card key={card.title} teaser noWrapper className="rounded shadow-lg col-lg-3 col-12 mr-4">
       <CardBody className="pb-5">
         <div className="mb-3 d-flex align-items-center">
           <img src={`/${card.icon}`} alt={`${card.alt}`} className={classes.logo} />
+          <span className="primary-color px-3 h3 mb-0">{card.title}</span>
         </div>
         <p className="card-text" dangerouslySetInnerHTML={{ __html: card.body }}></p>
       </CardBody>
