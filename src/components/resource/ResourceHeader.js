a4ef891322a180aab07236588acd5b9736be175b
emilioSp <emilio.spatola@gmail.com>
2021-03-24 11:55:19 +0100

    fix: risorse utili for mobile view

--- src/components/resource/ResourceHeader.js
+++ src/components/resource/ResourceHeader.js
@@ -2,7 +2,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 export const ResourceHeader = ({ title, classNames = '' }) => (
-  <h3 className={`text-uppercase font-weight-semibold h6 ${classNames}`}>{title}</h3>
+  <h3 className={`text-uppercase font-weight-semibold h6 text-center text-md-left ${classNames}`}>{title}</h3>
 );
 
 ResourceHeader.propTypes = {
--- src/components/resource/ResourceTitle.js
+++ src/components/resource/ResourceTitle.js
@@ -3,7 +3,7 @@ import PropTypes from 'prop-types';
 
 export const ResourceTitle = ({ title }) => (
   <div className="mx-0 mx-sm-4">
-    <h2 className="h5 font-weight-semibold text-info-title">{title}</h2>
+    <h2 className="h5 font-weight-semibold text-info-title text-center text-md-left">{title}</h2>
   </div>
 );
 
