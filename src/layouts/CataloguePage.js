f8423e2baf6e5fb518a13081fba4409aecf7a8f4
emilioSp <emilio.spatola@gmail.com>
2021-03-26 11:35:31 +0100

    fix: update catalogue page

--- src/layouts/CataloguePage.js
+++ src/layouts/CataloguePage.js
@@ -5,8 +5,9 @@ import { HeroTitle } from '../components/hero/HeroTitle.js';
 import { Hero } from '../components/hero/Hero.js';
 import { TextChunk } from '../components/TextChunk.js';
 import content from '../../contents/catalogue-page/catalogue.yml';
+import { HeroBody } from '../components/hero/HeroBody.js';
 
-const { title } = content;
+const { title, body } = content;
 
 const query = graphql`
   query {
@@ -28,6 +29,7 @@ export const CataloguePage = () => {
           <div className="offset-lg-1 col-lg-6 mt-4 mt-lg-0">
             <div className="text-center text-lg-left">
               <HeroTitle title={title} className="text-info" Tag="h1" />
+              <HeroBody html={body} />
             </div>
           </div>
         </div>
