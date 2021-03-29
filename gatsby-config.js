224c035336db09e59822e4835c689e2222528794
emilioSp <emilio.spatola@gmail.com>
2021-03-29 13:45:24 +0200

    fix: tracking

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,7 +3,7 @@ module.exports = {
     author: 'Dipartimento per la Trasformazione Digitale + AgID',
     description: 'L’adozione del modello cloud computing nelle amministrazioni italiane',
     hostname: 'cloud.italia.it',
-    siteUrl: `https://cloud.italia.it`,
+    siteUrl: 'https://cloud.italia.it',
     title: 'Cloud Italia',
     subtitle: 'Il Cloud della Pubblica Amministrazione',
   },
@@ -15,10 +15,21 @@ module.exports = {
       resolve: 'gatsby-plugin-matomo',
       options: {
         siteId: 'ZVQ0Q5N0dY',
-        matomoUrl: 'https://ingestion.webanalytics.italia.it/',
-        siteUrl: 'https://cloud-italia-i-git-test-analytics-dip-trasformazione-d-145d52.vercel.app/',
+        matomoUrl: 'https://ingestion.webanalytics.italia.it',
+        siteUrl: 'https://cloud.italia.it',
         matomoPhpScript: 'matomo.php',
         matomoJsScript: 'matomo.js',
+      },
+    },
+    {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        trackingId: 'UA-96140462-9',
+        // Defines where to place the tracking script - `true` in the head and `false` in the body
+        head: true,
+        // Setting this parameter is optional
+        anonymize: true,
+        defer: true,
       },
     },
     {
