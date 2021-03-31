f4383f66ca5700daaf4f1f8acf6d3243bf0b7ae4
Emilio Spatola <emilio.spatola@gmail.com>
2021-03-31 12:10:57 +0200

    Fix/accordion (#97)
    * fix: accordion should be close on init

--- src/components/glossary/GlossaryTerm.js
+++ src/components/glossary/GlossaryTerm.js
@@ -4,13 +4,14 @@ import PropTypes from 'prop-types';
 import { AccordionEntry } from '../AccordionEntry.js';
 import { glossaryTermPropTypes } from '../../utils/proptypes.js';
 
-export const GlossaryTerm = ({ term, expandedOnMount = false, scrollIntoView = false }) => {
-  const [expanded, setExpanded] = useState(expandedOnMount);
+export const GlossaryTerm = ({ term, expandedOnInit = false }) => {
+  const [expanded, setExpanded] = useState(expandedOnInit);
   useEffect(() => {
-    if (scrollIntoView) {
+    if (expandedOnInit) {
       document.getElementById(term.id)?.scrollIntoView({ block: 'center', inline: 'center' });
-    }
-  }, [term.id, scrollIntoView]);
+      setExpanded(true);
+    } // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []); // This is done on purpose because the automatic expansion could happen only on the first mount
   return (
     <Accordion id={term.id} className="bg-white shadow-lg my-0 my-lg-3 mx-0 mx-lg-2">
       <AccordionEntry
@@ -27,6 +28,5 @@ export const GlossaryTerm = ({ term, expandedOnMount = false, scrollIntoView = f
 
 GlossaryTerm.propTypes = {
   term: glossaryTermPropTypes,
-  expandedOnMount: PropTypes.bool,
-  scrollIntoView: PropTypes.bool,
+  expandedOnInit: PropTypes.bool,
 };
