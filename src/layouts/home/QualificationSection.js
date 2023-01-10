61b5c63d2b13d84d2f640aac396291c504268443
Gianvito Fanelli <gianvito.fanelli@teamdigitale.governo.it>
2023-01-10 12:24:40 +0100

    Aggiornamento contenuti

--- src/layouts/home/QualificationSection.js
+++ src/layouts/home/QualificationSection.js
@@ -63,26 +63,6 @@ export const QualificationSection = () => {
             <HeroTitle title={title} linkTo={services.linkTo} className="text-white" />
             <HeroBody html={body} />
           </div>
-          <HeroCtaContainer>
-            <Cta linkTo={services.linkTo} text={showMore} color="light" type="outline" aria-label={ctaAriaLabel} />
-            {/* <Cta linkTo={catalogue.linkTo} text={catalogue.label} color="light" /> */}
-            <div aria-hidden="true" className={classes.verticalDelimiter} />
-            <ExternalLink
-              linkTo={marketplace.linkTo}
-              ariaLabel={marketplace.ariaLabel}
-              className={`${classes.btnPrimaryLight} btn text-uppercase mx-4 ml-lg-0 my-2 btn-primary btn-icon focus-a11y-contrast`}
-            >
-              <span className="mr-3">{marketplace.label}</span>
-              <Icon
-                color="primary"
-                icon="it-external-link"
-                size="sm"
-                focusable={false}
-                role="img"
-                aria-label={ariaLabel.externalLink}
-              />
-            </ExternalLink>
-          </HeroCtaContainer>
         </div>
         <HeroGraphic className="col-lg-4 d-flex align-items-center justify-content-center">
           <StaticImage
@@ -93,26 +73,6 @@ export const QualificationSection = () => {
             formats={['AUTO', 'AVIF', 'WEBP']}
           />
         </HeroGraphic>
-      </div>
-      <div className="row mt-lg-2 mt-0">
-        <div className="col-12 text-center text-lg-left">
-          <ExternalLink
-            linkTo={openDataMarketplace.linkTo}
-            ariaLabel={openDataMarketplace.ariaLabel}
-            className={`btn-icon ${classes.whiteHighContrast}`}
-          >
-            <small>{openDataMarketplace.label}</small>
-            <Icon
-              className="ml-2"
-              icon="it-external-link"
-              size="sm"
-              color="white"
-              focusable={false}
-              role="img"
-              aria-label={ariaLabel.externalLink}
-            />
-          </ExternalLink>
-        </div>
       </div>
     </Hero>
   );
